3ec6f645b56a036c959c034674d3e90e4e67b3f0
allnulled <todosanulados@gmail.com>
2024-12-21 13:17:53 +0100

    new api, new readme, expanded example

--- README.md
+++ README.md
@@ -20,22 +20,28 @@ npm install -s @allnulled/importer
 
 ```js
 class Importer {
-    updateTotalModules(n) {/*...*/}
+    setTotal(n) {/*...*/}
+    setTimeout(ms) {/*...*/}
     async scriptSrc(src) {/*...*/}
     async scriptSrcModule(src) {/*...*/}
     async scriptAsync(url, context = {}) {/*...*/}
     async linkStylesheet(href) {/*...*/}
     async text(url) {/*...*/}
+    async importVueComponent(partialUrl) {/*...*/}
 }
 ```
+
+Just note, in the case of `importVueComponent`, it is for `vue@2` only, and accepts any resource that can be appended with `.js`, `.css` and `.html`, and each of the three completed URLs serves a file.
 
 ### 3) Call methods:
 
 ```js
+importer.setTotal(5);
 await importer.scriptSrc("test/scriptSrc.js");
 await importer.scriptSrcModule("test/scriptSrcModule.js");
 await importer.linkStylesheet("test/linkStylesheet.css");
 const txt = await importer.text("test/text.txt");
+await importer.importVueComponent("components/dir/resource") // + (.js & .css & .html)
 ```
 
 ## Full-example
@@ -44,7 +50,9 @@ From the JavaScript perspective, you can use the globally injected `importer` ob
 
 ```js
 Import_scripts: {
-    importer.updateTotalModules(19); // Number of total modules that must be loaded
+    importer.options.trace = true; // Set trace to true to see all the calls
+    importer.setTotal(19); // Number of total modules that must be loaded
+    importer.setTimeout(2000); // Wait 2 seconds untils remove the loader
     await Promise.all([
         importer.scriptSrc("src/external/socket.io-client.js"),
         importer.scriptSrc("src/external/vue-v2.js"),
@@ -138,5 +146,23 @@ const importer = Importer.create({
     id_trace: "#intersitial_modules_trace",
     id_loader: "#intersitial_loader",
     id_loader_bar: "#intersitial_loader_bar",
+    trace: false,
+    update_ui: false, // This option is only set to true on global: window.importer
+    update_ui_minimum_milliseconds: 1200, // Minimum milliseconds the importer
+    // must live before clearing the graphical counterpart
 })
+```
+
+You can use the parameters to set up your own graphical counterpart.
+
+Some of the settings are under `importer` and some under `importer.options`.
+
+Methods that start with `$` are considered private.
+
+To know the convenient number of modules, just see the logs in console, because they tell you each time a new module is loaded. Then hardcode the number in the code, and all fine.
+
+To fully trace the importer, you can do:
+
+```js
+importer.options.trace = true;
 ```
--- importer.js
+++ importer.js
@@ -4,6 +4,15 @@ const Importer = class {
     return new this(...args);
   }
 
+  $trace(method, args) {
+    if(this.options.trace) {
+      const args_resumen = Array.from(args).map((it, index) => {
+        return (index+1) + "=" + typeof it;
+      }).join(", ");
+      console.log("[TRACE][importer][" + method + "][" + args.length + "][" + args_resumen + "]");
+    }
+  }
+
   constructor(total_modules = 0, options_input = {}) {
     const options = Object.assign({}, {
       id: "#intersitial",
@@ -12,92 +21,150 @@ const Importer = class {
       id_trace: "#intersitial_modules_trace",
       id_loader: "#intersitial_loader",
       id_loader_bar: "#intersitial_loader_bar",
+      trace: false,
+      update_ui: false,
+      update_ui_minimum_milliseconds: 1200,
     }, options_input);
-    this.id = options.id;
-    this.id_all = options.id_all;
-    this.id_loaded = options.id_loaded;
-    this.id_trace = options.id_trace;
-    this.id_loader = options.id_loader;
-    this.id_loader_bar = options.id_loader_bar;
+    this.options = options;
+    Duplicated_options: {
+      this.id = options.id;
+      this.id_all = options.id_all;
+      this.id_loaded = options.id_loaded;
+      this.id_trace = options.id_trace;
+      this.id_loader = options.id_loader;
+      this.id_loader_bar = options.id_loader_bar;
+    }
     this.modules_total = total_modules;
     this.modules_loaded = -1;
     this.time_of_creation = new Date();
     this.is_loaded = false;
-    this.updateTotalModules();
+    this.setTotal();
+    this.$trace("constructor", arguments);
   }
 
-  getMillisecondsOfLife() {
-    return this.formatMilliseconds(new Date() - this.time_of_creation);
+  $getMillisecondsOfLife() {
+    this.$trace("$getMillisecondsOfLife", arguments);
+    return this.$formatMilliseconds(new Date() - this.time_of_creation);
   }
 
-  formatMilliseconds(ms) {
+  $formatMilliseconds(ms) {
+    this.$trace("$formatMilliseconds", arguments);
     return ms.toLocaleString("es-ES");
   }
 
-  updateTotalModules(total = undefined) {
+  setTimeout(timeout) {
+    this.$trace("$setTimeout", arguments);
+    this.options.update_ui_minimum_milliseconds = timeout;
+    return this;
+  }
+
+  setTotal(total = undefined) {
+    this.$trace("$setTotal", arguments);
     try {
       if(typeof total !== "undefined") {
         this.modules_total = total;
       }
+      if(!this.options.update_ui) {
+        return;
+      }
       const htmlTotal = document.querySelector(this.id_all);
       htmlTotal.textContent = this.modules_total;
+      return this;
     } catch (error) {
+      console.log(error);
       console.log("[WARN][Importer] Cannot update total modules. Insert «" + this.id_all + "» to skip this warning.");
     }
   }
 
-  prependTrace(message) {
+  $prependTrace(message) {
+    this.$trace("$prependTrace", arguments);
     try {
+      if(!this.options.update_ui) {
+        return;
+      }
       const htmlTrace = document.querySelector(this.id_trace);
       if(htmlTrace.textContent.length) {
         htmlTrace.textContent = "\n" + htmlTrace.textContent;
       }
       htmlTrace.textContent = message + htmlTrace.textContent;
     } catch (error) {
+      console.log(error);
       console.log("[WARN][Importer] Cannot append trace. Insert «" + this.id_trace + "» to skip this warning.");
     }
   }
 
-  increaseLoadedModules(moduleType = "unknown", moduleId = "unknown") {
+  $increaseLoadedModules(moduleType = "unknown", moduleId = "unknown") {
+    this.$trace("$increaseLoadedModules", arguments);
     try {
-      console.log(`[OK][Importer] Loaded «${moduleId}» as «${moduleType}» ${this.getMillisecondsOfLife()}`);
+      console.log(`[OK][Importer] Loaded «${moduleId}» as «${moduleType}» ${this.$getMillisecondsOfLife()}`);
+      ++this.modules_loaded;
+      if(!this.options.update_ui) {
+        return;
+      }
       const htmlLoaded = document.querySelector(this.id_loaded);
-      htmlLoaded.textContent = ++this.modules_loaded;
-      this.prependTrace(`Loaded «${moduleType}» from «${moduleId}»`);
-      this.updateLoaderBar();
+      htmlLoaded.textContent = this.modules_loaded;
+      this.$prependTrace(`Loaded «${moduleType}» from «${moduleId}»`);
+      this.$updateLoaderBar();
       if((this.modules_loaded+1) >= this.modules_total) {
-        this.removeIntersitial();
+        this.$removeIntersitial();
       }
     } catch (error) {
       if(this.is_loaded) {
-        console.log("[CAUTION][Importer] Module out of the count was loaded: type «" + moduleType + "» from «" + moduleId + "».");
+        console.log("[CAUTION][Importer] Module out of the count was loaded: type «" + moduleType + "» from «" + moduleId + "» [" + this.modules_loaded + " modules loaded].");
       } else {
+        console.log(error);
         console.log("[WARN][Importer] Cannot update total modules. Insert «" + this.id_loaded + "» to skip this warning.");
       }
     }
   }
 
-  updateLoaderBar() {
+  $updateLoaderBar() {
+    this.$trace("$updateLoaderBar", arguments);
     try {
+      if(!this.options.update_ui) {
+        return;
+      }
       const htmlLoaderBar = document.querySelector(this.id_loader_bar);
       const percentageCompleted = Math.round((this.modules_loaded / this.modules_total) * 100);
       htmlLoaderBar.style.width = percentageCompleted + "%";
     } catch (error) {
+      console.log(error);
       console.log("[WARN][Importer] Cannot update loaded bar. Insert «" + this.id_loader_bar + "» inside of «" + this.id_loader + "» to skip this warning.");
     }
   }
 
-  removeIntersitial() {
+  $removeIntersitial() {
+    this.$trace("$removeIntersitial", arguments);
     try {
+      if(this.is_loaded) {
+        return;
+      }
       this.is_loaded = true;
-      const intersitial = document.querySelector(this.id);
-      intersitial.remove();
+      if(!this.options.update_ui) {
+        return;
+      }
+      if(this.options.update_ui_minimum_milliseconds) {
+        clearTimeout(this.options.update_ui_minimum_milliseconds_timeout_id);
+        this.update_ui_minimum_milliseconds_timeout_id = setTimeout(() => {
+          const intersitial = document.querySelector(this.id);
+          if(intersitial) {
+            intersitial.remove();
+          } else {
+            console.log("Calling to $removeIntersitial when there is not such");
+          }
+        }, this.options.update_ui_minimum_milliseconds);
+      } else {
+        const intersitial = document.querySelector(this.id);
+        intersitial.remove();
+      }
     } catch (error) {
+      console.log(error);
       console.log("[WARN][Importer] Cannot remove intersitial. Insert «" + this.id + "» to skip this warning.");
     }
   }
 
   async scriptSrc(src) {
+    this.$trace("scriptSrc", arguments);
     await new Promise((resolve, reject) => {
       const script = document.createElement("script");
       script.src = src;
@@ -105,11 +172,12 @@ const Importer = class {
       script.onerror = (e) => reject(e);
       document.head.appendChild(script);
     });
-    this.increaseLoadedModules("script.src", src);
+    this.$increaseLoadedModules("script.src", src);
     return;
   }
 
   async scriptSrcModule(src) {
+    this.$trace("scriptSrcModule", arguments);
     await new Promise((resolve, reject) => {
       const script = document.createElement("script");
       script.src = src;
@@ -118,22 +186,24 @@ const Importer = class {
       script.onerror = (e) => reject(e);
       document.head.appendChild(script);
     });
-    this.increaseLoadedModules("script.src.module", src);
+    this.$increaseLoadedModules("script.src.module", src);
     return;
   }
 
   async scriptAsync(url, context = {}) {
+    this.$trace("scriptAsync", arguments);
     const response = await fetch(url);
     if (!response.ok) throw new Error(`Failed to fetch script: ${url}`);
     const scriptText = await response.text();
     const AsyncFunction = (async function () { }).constructor;
     const asyncFunction = new AsyncFunction(...Object.keys(context), scriptText);
     const result = await asyncFunction(...Object.values(context));
-    this.increaseLoadedModules("script.async", url);
+    this.$increaseLoadedModules("script.async", url);
     return result;
   }
 
   async linkStylesheet(href) {
+    this.$trace("linkStylesheet", arguments);
     await new Promise((resolve, reject) => {
       const link = document.createElement("link");
       link.rel = "stylesheet";
@@ -143,18 +213,20 @@ const Importer = class {
       link.onerror = (e) => reject(e);
       document.head.appendChild(link);
     });
-    this.increaseLoadedModules("link.stylesheet.css", href);
+    this.$increaseLoadedModules("link.stylesheet.css", href);
     return;
   }
 
   async text(url) {
+    this.$trace("text", arguments);
     const response = await fetch(url);
-    this.increaseLoadedModules("text", url);
+    this.$increaseLoadedModules("text", url);
     if (!response.ok) throw new Error(`Failed to fetch text: ${url}`);
     return response.text();
   }
 
   async importVueComponent(url) {
+    this.$trace("importVueComponent", arguments);
     try {
       const urlJs = url + ".js";
       const urlCss = url + ".css";
@@ -162,7 +234,7 @@ const Importer = class {
       const template = await this.text(urlHtml);
       await this.scriptAsync(urlJs, { $template: template });
       await this.linkStylesheet(urlCss);
-      this.increaseLoadedModules("vue.component", url);
+      this.$increaseLoadedModules("vue.component", url);
     return;
   } catch (error) {
       console.log(error);
@@ -174,3 +246,4 @@ const Importer = class {
 
 window.Importer = Importer;
 window.importer = new Importer();
+window.importer.options.update_ui = true;
