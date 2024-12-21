872ecf3dec709792638dd7b1fa78870997e99be4
allnulled <todosanulados@gmail.com>
2024-12-21 11:19:54 +0100

    new api

--- README.md
+++ README.md
@@ -20,20 +20,123 @@ npm install -s @allnulled/importer
 
 ```js
 class Importer {
-    static async scriptSrc(src) {/*...*/}
-    static async scriptSrcModule(src) {/*...*/}
-    static async scriptAsync(url, context = {}) {/*...*/}
-    static async linkStylesheet(href) {/*...*/}
-    static async text(url) {/*...*/}
+    updateTotalModules(n) {/*...*/}
+    async scriptSrc(src) {/*...*/}
+    async scriptSrcModule(src) {/*...*/}
+    async scriptAsync(url, context = {}) {/*...*/}
+    async linkStylesheet(href) {/*...*/}
+    async text(url) {/*...*/}
 }
 ```
 
 ### 3) Call methods:
 
 ```js
-await Importer.scriptSrc("test/scriptSrc.js");
-await Importer.scriptSrcModule("test/scriptSrcModule.js");
-await Importer.linkStylesheet("test/linkStylesheet.css");
-const txt = await Importer.text("test/text.txt");
+await importer.scriptSrc("test/scriptSrc.js");
+await importer.scriptSrcModule("test/scriptSrcModule.js");
+await importer.linkStylesheet("test/linkStylesheet.css");
+const txt = await importer.text("test/text.txt");
 ```
 
+## Full-example
+
+From the JavaScript perspective, you can use the globally injected `importer` object like so:
+
+```js
+Import_scripts: {
+    importer.updateTotalModules(19); // Number of total modules that must be loaded
+    await Promise.all([
+        importer.scriptSrc("src/external/socket.io-client.js"),
+        importer.scriptSrc("src/external/vue-v2.js"),
+        importer.scriptSrc("src/external/basic-logger.js"),
+        importer.scriptSrc("src/external/ensure.js"),
+        importer.scriptSrc("src/external/webmarket.js"),
+        importer.importVueComponent("src/components/c-title/c-title"),
+        importer.importVueComponent("src/components/home-page/home-page"),
+        importer.importVueComponent("src/components/app/app"),
+    ]);
+    await importer.linkStylesheet("src/external/win7.css");
+    await importer.scriptSrc("src/external/refresher.js");
+}
+```
+
+From the Html perspective, you should better insert something like this:
+
+```html
+    <div id="intersitial">
+        <div id="intersitial_loader">
+            <div id="intersitial_loader_bar"></div>
+            <div id="intersitial_message">
+                <span>Loaded <span id="intersitial_modules_loaded"></span> out of <span id="intersitial_modules_all"></span> modules</span>
+            </div>
+        </div>
+        <pre  id="intersitial_modules_trace"></pre>
+    </div>
+```
+
+From the Css perspective, you maybe find useful something like this:
+
+```html
+<style>
+    #intersitial {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        font-family: Arial;
+        font-size: 12px;
+    }
+    #intersitial_message {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        text-align: center;
+        font-size: 12px;
+        color: gold:
+        font-family: Arial;
+        font-size: 12px;
+        border: 1px solid #888;
+    }
+    #intersitial_loader {
+        position: relative;
+        font-family: Arial;
+        min-height: 14px;
+        background-color: #333;
+    }
+    #intersitial_loader_bar {
+        height: 100%;
+        min-height: 15px;
+        font-size: 12px;
+        background-color: #0F0;
+        width: 0%;
+    }
+    #intersitial_modules_trace {
+        font-family: Arial;
+        font-size: 12px;
+        border: 1px solid #888;
+        margin: 0;
+        padding: 4px;
+        margin-bottom: 1px;
+    }
+</style>
+```
+
+The intersitial will be removed once all the modules are loaded.
+
+## Custom usage
+
+If you need, for any reason, another importer, you can simply:
+
+```js
+const importer = Importer.create({
+    /* Options of the API: default values */
+    id: "#intersitial",
+    id_loaded: "#intersitial_modules_loaded",
+    id_all: "#intersitial_modules_all",
+    id_trace: "#intersitial_modules_trace",
+    id_loader: "#intersitial_loader",
+    id_loader_bar: "#intersitial_loader_bar",
+})
+```
--- importer.js
+++ importer.js
@@ -1,17 +1,116 @@
 const Importer = class {
 
-  static async scriptSrc(src) {
-    return new Promise((resolve, reject) => {
+  static create(...args) {
+    return new this(...args);
+  }
+
+  constructor(total_modules = 0, options_input = {}) {
+    const options = Object.assign({}, {
+      id: "#intersitial",
+      id_loaded: "#intersitial_modules_loaded",
+      id_all: "#intersitial_modules_all",
+      id_trace: "#intersitial_modules_trace",
+      id_loader: "#intersitial_loader",
+      id_loader_bar: "#intersitial_loader_bar",
+    }, options_input);
+    this.id = options.id;
+    this.id_all = options.id_all;
+    this.id_loaded = options.id_loaded;
+    this.id_trace = options.id_trace;
+    this.id_loader = options.id_loader;
+    this.id_loader_bar = options.id_loader_bar;
+    this.modules_total = total_modules;
+    this.modules_loaded = -1;
+    this.time_of_creation = new Date();
+    this.is_loaded = false;
+    this.updateTotalModules();
+  }
+
+  getMillisecondsOfLife() {
+    return this.formatMilliseconds(new Date() - this.time_of_creation);
+  }
+
+  formatMilliseconds(ms) {
+    return ms.toLocaleString("es-ES");
+  }
+
+  updateTotalModules(total = undefined) {
+    try {
+      if(typeof total !== "undefined") {
+        this.modules_total = total;
+      }
+      const htmlTotal = document.querySelector(this.id_all);
+      htmlTotal.textContent = this.modules_total;
+    } catch (error) {
+      console.log("[WARN][Importer] Cannot update total modules. Insert «" + this.id_all + "» to skip this warning.");
+    }
+  }
+
+  prependTrace(message) {
+    try {
+      const htmlTrace = document.querySelector(this.id_trace);
+      if(htmlTrace.textContent.length) {
+        htmlTrace.textContent = "\n" + htmlTrace.textContent;
+      }
+      htmlTrace.textContent = message + htmlTrace.textContent;
+    } catch (error) {
+      console.log("[WARN][Importer] Cannot append trace. Insert «" + this.id_trace + "» to skip this warning.");
+    }
+  }
+
+  increaseLoadedModules(moduleType = "unknown", moduleId = "unknown") {
+    try {
+      console.log(`[OK][Importer] Loaded «${moduleId}» as «${moduleType}» ${this.getMillisecondsOfLife()}`);
+      const htmlLoaded = document.querySelector(this.id_loaded);
+      htmlLoaded.textContent = ++this.modules_loaded;
+      this.prependTrace(`Loaded «${moduleType}» from «${moduleId}»`);
+      this.updateLoaderBar();
+      if((this.modules_loaded+1) >= this.modules_total) {
+        this.removeIntersitial();
+      }
+    } catch (error) {
+      if(this.is_loaded) {
+        console.log("[CAUTION][Importer] Module out of the count was loaded: type «" + moduleType + "» from «" + moduleId + "».");
+      } else {
+        console.log("[WARN][Importer] Cannot update total modules. Insert «" + this.id_loaded + "» to skip this warning.");
+      }
+    }
+  }
+
+  updateLoaderBar() {
+    try {
+      const htmlLoaderBar = document.querySelector(this.id_loader_bar);
+      const percentageCompleted = Math.round((this.modules_loaded / this.modules_total) * 100);
+      htmlLoaderBar.style.width = percentageCompleted + "%";
+    } catch (error) {
+      console.log("[WARN][Importer] Cannot update loaded bar. Insert «" + this.id_loader_bar + "» inside of «" + this.id_loader + "» to skip this warning.");
+    }
+  }
+
+  removeIntersitial() {
+    try {
+      this.is_loaded = true;
+      const intersitial = document.querySelector(this.id);
+      intersitial.remove();
+    } catch (error) {
+      console.log("[WARN][Importer] Cannot remove intersitial. Insert «" + this.id + "» to skip this warning.");
+    }
+  }
+
+  async scriptSrc(src) {
+    await new Promise((resolve, reject) => {
       const script = document.createElement("script");
       script.src = src;
       script.onload = () => resolve();
       script.onerror = (e) => reject(e);
       document.head.appendChild(script);
     });
+    this.increaseLoadedModules("script.src", src);
+    return;
   }
 
-  static async scriptSrcModule(src) {
-    return new Promise((resolve, reject) => {
+  async scriptSrcModule(src) {
+    await new Promise((resolve, reject) => {
       const script = document.createElement("script");
       script.src = src;
       script.type = "module";
@@ -19,33 +118,59 @@ const Importer = class {
       script.onerror = (e) => reject(e);
       document.head.appendChild(script);
     });
+    this.increaseLoadedModules("script.src.module", src);
+    return;
   }
 
-  static async scriptAsync(url, context = {}) {
+  async scriptAsync(url, context = {}) {
     const response = await fetch(url);
     if (!response.ok) throw new Error(`Failed to fetch script: ${url}`);
     const scriptText = await response.text();
+    const AsyncFunction = (async function () { }).constructor;
     const asyncFunction = new AsyncFunction(...Object.keys(context), scriptText);
-    return asyncFunction(...Object.values(context));
+    const result = await asyncFunction(...Object.values(context));
+    this.increaseLoadedModules("script.async", url);
+    return result;
   }
 
-  static async linkStylesheet(href) {
-    return new Promise((resolve, reject) => {
+  async linkStylesheet(href) {
+    await new Promise((resolve, reject) => {
       const link = document.createElement("link");
       link.rel = "stylesheet";
+      link.type = "text/css";
       link.href = href;
       link.onload = () => resolve();
       link.onerror = (e) => reject(e);
       document.head.appendChild(link);
     });
+    this.increaseLoadedModules("link.stylesheet.css", href);
+    return;
   }
 
-  static async text(url) {
+  async text(url) {
     const response = await fetch(url);
+    this.increaseLoadedModules("text", url);
     if (!response.ok) throw new Error(`Failed to fetch text: ${url}`);
     return response.text();
   }
-  
+
+  async importVueComponent(url) {
+    try {
+      const urlJs = url + ".js";
+      const urlCss = url + ".css";
+      const urlHtml = url + ".html";
+      const template = await this.text(urlHtml);
+      await this.scriptAsync(urlJs, { $template: template });
+      await this.linkStylesheet(urlCss);
+      this.increaseLoadedModules("vue.component", url);
+    return;
+  } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
 }
 
 window.Importer = Importer;
+window.importer = new Importer();
